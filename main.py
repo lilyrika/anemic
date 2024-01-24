f0725671754d1e7ceafa6cd895e7fe62ad41e492
lilyrika <115484695+lilyrika@users.noreply.github.com>
2024-01-24 13:30:27 +0000

    Updated

--- main.py
+++ main.py
@@ -1,51 +1,64 @@
 import sqlite3 as sql
 
-    # def create_artists_table(self):
-    #     self.command("DROP TABLE IF EXISTS Artists")
-    #     self.command(
-    #         """
-    #         CREATE TABLE Artists(
-    #         artistid INT PRIMARY KEY,
-    #         name VARCHAR(64)
-    #         )
-    #         """
-    #     )
+#region Create Tables
+# database.command("DROP TABLE IF EXISTS Artists")
+# database.command(
+#     """
+#     CREATE TABLE Artists(
+#     artistid INT PRIMARY KEY,
+#     name VARCHAR(64)
+#     )
+#     """
+# )
 
-    # def create_albums_table(self):
-    #     self.command("DROP TABLE IF EXISTS Albums")
-    #     self.command(
-    #         """
-    #         CREATE TABLE Albums(
-    #         albumid INT PRIMARY KEY NOT NULL,
-    #         name VARCHAR(64) NOT NULL,
-    #         artist VARCHAR(64) NOT NULL,
-    #         genre1 VARCHAR(64),
-    #         genre2 VARCHAR(64),
-    #         genre3 VARCHAR(64)
-    #         )
-    #         """
-    #     )
+# database.command("DROP TABLE IF EXISTS Albums")
+# database.command(
+#     """
+#     CREATE TABLE Albums(
+#     albumid INT PRIMARY KEY NOT NULL,
+#     name VARCHAR(64) NOT NULL,
+#     artist VARCHAR(64) NOT NULL
+#     )
+#     """
+# )
+
+# database.command("DROP TABLE IF EXISTS Genres")
+# database.command(
+#     """
+#     CREATE Table Genres(
+#     genreid INT PRIMARY KEY NOT NULL, 
+#     albumid INT NOT NULL, 
+#     genre VARCHAR(64) NOT NULL
+#     )
+#     """
+# )
+#endregion
 
 class Database:
     def __init__(self):
         self.conn = sql.connect("database.db")
         self.cursor = self.conn.cursor()
     
-    def command(self, text):
-        self.cursor.execute(text)
-    
-    def add_album(self):
-        self.command(
+    def add_album(self, name, artist):
+        command = """
+        INSERT INTO Albums (albumid, name, artist)
+        SELECT MAX(albumid)+1, ?, ? FROM Genres
         """
-        INSERT INTO Albums (albumid, name, artist, genre1, genre2) 
-        SELECT MAX(albumid)+1, 'SCARING THE HOES', 'JPEGMAFIA', 'Experimental Hip Hop', 'Hardcore Hip Hop' FROM Albums
-        """
-        )
 
+        self.cursor.execute(command, (name, artist))
+        database.conn.commit()
+    
+    def add_genre(self, albumid, genre):
+        command = """
+        INSERT INTO Genres (genreid, albumid, genre)
+        SELECT MAX(genreid)+1, ?, ? FROM Genres
+        """
+
+        self.cursor.execute(command, (albumid, genre))
         database.conn.commit()
 
-
-
-
 database = Database()
-database.command("DELETE FROM Albums where albumid=9")
+database.add_album("To Be Kind", "Swans")
+database.add_genre(2, "Post-Rock")
+
+#
