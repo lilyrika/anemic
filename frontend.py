5cc9cf1942745b21b1d5fca528cc1c4c5c2ff5ff
lilyrika <115484695+lilyrika@users.noreply.github.com>
2024-09-06 13:56:27 +0100

    do your frontend

--- frontend.py
+++ frontend.py
@@ -1,10 +1,16 @@
 import tkinter as tk
 from tkinter import ttk
 
-window = tk.Tk()
-window.geometry("1280x720")
+root = tk.Tk()
+root.geometry("1280x720")
+root.configure(background="#0b0f14")
+root.title("Anemic")
 
-searchbar = tk.Entry(window)
-searchbar.pack()
+title = tk.Label(root, text = "Welcome to Anemic!", font = "Bahnschrift", anchor = "w")
+title.grid(row = 1, column = 1)
 
-window.mainloop()
+searchbar_var = tk.StringVar()
+searchbar = tk.Entry(root, width=90)
+searchbar.grid(row = 2, column = 1, padx=10, pady=10)
+
+root.mainloop()
