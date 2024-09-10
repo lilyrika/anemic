e8dc84e6bd58549b3a2c15073ad849c6fa96dc53
lilyrika <115484695+lilyrika@users.noreply.github.com>
2024-09-10 10:21:47 +0100

    make the front end work

--- frontend.py
+++ frontend.py
@@ -7,10 +7,13 @@ root.configure(background="#0b0f14")
 root.title("Anemic")
 
 title = tk.Label(root, text = "Welcome to Anemic!", font = "Bahnschrift", anchor = "w")
-title.grid(row = 1, column = 1)
+title.place(x=10, y=10)
 
 searchbar_var = tk.StringVar()
-searchbar = tk.Entry(root, width=90)
-searchbar.grid(row = 2, column = 1, padx=10, pady=10)
+searchbar = tk.Entry(root, width=60)
+searchbar.place(x=165, y=13)
+
+searchbutton = tk.Button(root, text = "Search")
+searchbutton.place(x=530, y=10)
 
 root.mainloop()
