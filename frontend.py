26407aa57ea7225854f493ef633812bf813c7903
lilyrika <115484695+lilyrika@users.noreply.github.com>
2024-09-12 12:25:02 +0100

    searchbar var

--- frontend.py
+++ frontend.py
@@ -1,19 +1,37 @@
+from backend import Database
+
 import tkinter as tk
-from tkinter import ttk
+from tkinter import ttk 
+
+database = Database()
+
+def cur(cmd): 
+    database.cur.execute(cmd)
+
+def wipe():
+    cur("DELETE FROM Albums")
+    cur("DELETE FROM Ratings")
+    cur("DELETE FROM Genres")
+
+###############
 
 root = tk.Tk()
 root.geometry("1280x720")
 root.configure(background="#0b0f14")
 root.title("Anemic")
 
-title = tk.Label(root, text = "Welcome to Anemic!", font = "Bahnschrift", anchor = "w")
+#implement tabs
+
+title = tk.Label(root, text="Welcome to Anemic!", font="Bahnschrift", anchor="w")
 title.place(x=10, y=10)
 
-searchbar_var = tk.StringVar()
 searchbar = tk.Entry(root, width=60)
 searchbar.place(x=165, y=13)
 
-searchbutton = tk.Button(root, text = "Search")
+def search():
+    query = searchbar.get()
+
+searchbutton = ttk.Button(root, text="Search", command=search)
 searchbutton.place(x=530, y=10)
 
 root.mainloop()
